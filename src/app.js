86c306c17ac48d42f5ce4925adb8880b277dd152
Gourav Singh Rawat <grvrawat999@gmail.com>
2021-11-29 17:43:09 +0530

    made routing with staticPath and made carousel
    main changes in main.js and index.html
    create footer after this.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
+const path = require("path");
 
+// statics path
+const staticPath = path.join(__dirname, "../public");
+app.use(express.static(staticPath));
+
+// routing
 app.get("", (req, res) => {
-    res.send("Welcome to my Page");
+    res.send("");
 });
+
+app.get("/about", (req, res) => {
+    res.send("About page");
+});
+
+app.get("/porjects", (req, res) => {
+    res.send("Projects");
+});
+
+app.get("*", (req, res) => {
+    res.send("404 error page...");
+});
+
 
 app.listen(port, () => {
     console.log(`listening to the port ${port}`);
