2b347eacf0475e3409a7288195bdbd5cef062afc
Gourav Singh Rawat <grvrawat999@gmail.com>
2021-11-29 23:40:07 +0530

    removed static pages and added hbs template and navbar partial (added animation in projects)
    i want to sleep :(   zzzz

--- src/app.js
+++ src/app.js
@@ -1,27 +1,37 @@
 const express = require("express");
+const { template } = require("handlebars");
 const app = express();
-const port = process.env.PORT || 8000;
 const path = require("path");
+const hbs = require("hbs");
+const port = process.env.PORT || 8000;
 
 // statics path
 const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// using template engines instead of static pages
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+hbs.registerPartials(partialsPath);
+
 app.use(express.static(staticPath));
 
 // routing
 app.get("", (req, res) => {
-    res.send("");
+    res.render("index");
 });
 
 app.get("/about", (req, res) => {
-    res.send("About page");
+    res.render("about")
 });
 
 app.get("/projects", (req, res) => {
-    res.send("Projects");
+    res.render("projects");
 });
 
 app.get("*", (req, res) => {
-    res.send("404 error page...");
+    res.render("404");
 });
 
 
